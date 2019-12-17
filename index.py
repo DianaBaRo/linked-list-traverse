35892920fb3bce0e4d752366de49a895157df4c2
Diana Badas <diana.badas@gmail.com>
2019-12-17 16:41:34 +0000

    traverse

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
+#traverse
 
+# O(N), linear time complexity
+
+def insertEnd(self, data):
+    self.size = self.size + 1;
+    newNode = Node(data);
+    actualNode = self.head;
+
+    while actualNode.nextNode is not None:
+        actualNode = actualNode.nextNode;
+
+    actualNode.nextNode = newNode;
+
+def traverseList(self):
+    actualNode = self.head;
+
+    while actualNode is not None:
+        print("%d " % actualNode.data);
+        actualNode = actualNode.nextNode;
